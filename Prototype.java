6a819e1cd77e4b3cf65debe80e5e439a1cb9304f
JoshRoxas <aaronjoshuaroxas678@gmail.com>
2020-11-03 13:32:02 +0800

    Add files via upload

---
+++ Prototype.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Prototype
+{
+	
+	public static void main(String[] args) 
+	{
+		//Variables
+	    Scanner s = new Scanner(System.in);
+	    Random r = new Random();
+		String monster;
+	    int playerHP = 100;
+		
+		//Introduction to the game
+	    
+	    System.out.println("--------------------------------------");
+	    System.out.println("Welcome to Shadow's Embrace");
+	    System.out.print("Please enter your name: ");
+	    String user = s.nextLine();
+	    System.out.println("Welcome  " + user + " Please press ENTER to start");
+	    
+
+	    //START OF GAME
+	    System.out.println(""); 
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
