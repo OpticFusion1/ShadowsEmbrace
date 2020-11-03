db7dcb692715c96f05eed9a401049a7d438eded9
JoshRoxas <aaronjoshuaroxas678@gmail.com>
2020-11-03 13:55:33 +0800

    Update Prototype.java

--- Prototype.java
+++ Prototype.java
@@ -2,31 +2,34 @@ import java.util.Random;
 import java.util.Scanner;
 public class Prototype
 {
-	
+	//Variables
+    Scanner s = new Scanner(System.in);
+    Random r = new Random();
+    int monsterHP = 15;
+    int playerHP = 100;
+    int playerChoice;
+    String user;
+
 	public static void main(String[] args) 
 	{
-		//Variables
-	    Scanner s = new Scanner(System.in);
-	    Random r = new Random();
-	    int monsterHP = 15;
-	    int playerHP = 100;
-	    int playerChoice;
-		
-		//Introduction to the game
-	    
-	    System.out.println("--------------------------------------");
-	    System.out.println("Welcome to Shadow's Embrace");
-	    System.out.print("Please enter your name: ");
-	    String user = s.nextLine();
-	    System.out.println("Welcome  " + user + " Please press ENTER to start");   	
+		Prototype game = new Prototype();
+		game.playerSetup();
+		game.gameStart();
 	}
 
+	public void playerSetup()
+	{
+	    System.out.println("--------------------------------------"); 
+	    System.out.println("Welcome to Shadow's Embrace. Your adventure begins here. Please enter a name: ");
+	}
+	
 	public void gameStart()
 	{
 		 //START OF GAME
 	    System.out.println("--------------------------------------"); 
-	    System.out.println("");
+		System.out.println( " You are a man fighting for your love of your life!");
 	}
 	
 
 }	
+
