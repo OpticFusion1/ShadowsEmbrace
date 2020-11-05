c7749372699e4c69efae34bbec7f5c7ab88a22d5
JoshRoxas <aaronjoshuaroxas678@gmail.com>
2020-11-05 11:42:12 +0800

    Update Prototype.java

--- Prototype.java
+++ Prototype.java
@@ -1,109 +1,151 @@
-import java.util.Random;
 import java.util.Scanner;
-public class Prototype
+import java.util.Random;
+public class ShadowsEmbrace 
 {
-	//System Variables
-    Scanner s = new Scanner(System.in);
-    Random r = new Random();
-    int playerChoice;
-    int subChoice;
-    String user;
-    
-    //Player Variables
-    int heal = 3;
-    int playerHP = 100;
-    int attackDamage = r.nextInt(30);
-    String playerWeapon;
-    
-    //Items
-    int key;
-    
-    //Monster/Enemy Variables
-    int monsterAttack[] = {5,10,15,20,25};
-    int monsterHP = 50;
-    int monsterCount = 10;
-    String monster[] = {"Skeleton", "Warrior" , "Archer"};
-    int monsterRandomizer = r.nextInt(monster.length);
-    
 	
+	
+	Random r = new Random();
+	Scanner s = new Scanner(System.in);
+	Scanner enterScanner = new Scanner(System.in);
+	int playerHP = 100;
+	String playerName;
+	int choice;
+	int monsterHP = r.nextInt(25);
+	String monster[] = {"Skeleton", "Warrior" , "Archer"};
+        int monsterRandomizer = r.nextInt(monster.length);
+	int monsterCounter = 3;
+	int key;
+		
 	public static void main(String[] args) 
 	{
+
+ 
+		ShadowsEmbrace	game = new ShadowsEmbrace();	
 		
-	    Prototype game = new Prototype();
-	    game.playerSetup();
-	    game.gameStart();
-	    game.monsterFight();
-
-
+		game.playerSetUp(); 	
+		game.startUp();
 	}
-
-	public void playerSetup()
-	{
+	
+	public void playerSetUp(){
 		
 	    System.out.println("------------------------------------------------"); 
 	    System.out.println("\nWelcome to Shadow's Embrace.");
 	    System.out.print("Your adventure begins here. Please enter a name: ");
-	    user = s.nextLine();
+		
+		playerName = s.nextLine();
+		System.out.println();
+		System.out.println("Hello " + playerName + ", let's start the game!");	
+		
+		
+	}	
+	
+	public void startUp(){
+		
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println(playerName +" You are a man fighting for Nessa. You must rescue her in order for you to successfully finish this mission!");
+		System.out.println("But beware of dangerous underground creatures that you may encounter!");
+		System.out.println("You need to defeat 3 boss in order for you to sucessfully retrieve her");
+		System.out.println("What do you want to do?");
+		System.out.println("");
+		System.out.println("1: Continue Walking");
+		System.out.println("2: Exit Game");
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.print("Select: ");
+		choice = s.nextInt();
+		
+		if(choice==1)
+		{
+			monsterFight();		
+		}
+			
+		
+		else if(choice==2)
+		{
+			System.out.println("You left the game.");
+		}
+		
+		
+		else
+		{
+			startUp();
+		}
 	}
 	
-	public void gameStart()
-	{
-		 //START OF GAME
-	 	System.out.println("\n------------------------------------------------"); 
-	 	System.out.println(user +" You are a man fighting for your love of your life! You must rescue her in order for you to successfully finish this mission!");
-       	 	System.out.println("(Press ENTER to proceed.)");
-	 	s.nextLine();		
-		
-	}
+	
+	
+	
 	
 	
 	public void monsterFight()
 	{
-		monsterHP = 50;
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.println("You encountered " + monster[monsterRandomizer]);
-		System.out.println("Monster HP: " + monsterHP);
-		System.out.println("\n1: Attack");
-		System.out.println("2: Run");
-		System.out.println("\n------------------------------------------------------------------\n");
-		
-		subChoice = s.nextInt();
-		
-		if(subChoice==1)
+		while (monsterCounter > 0)
 		{
-			attack();
-		}
-		else if(subChoice==2)
-		{
-			System.out.println("There's no running in here! its either you fight or you die.");
-			System.out.println("The " + monster[monsterRandomizer] + " killed you");
-			dead();
-		}
-		else
-		{	
-			monsterFight();
-		}
+			monsterCounter--;
+			System.out.println("\n------------------------------------------------------------------\n");
+			System.out.println("You encountered a " + monster[monsterRandomizer] + "!\n");
+			System.out.println("Choose:");
+			System.out.println("1: Fight");
+			System.out.println("2: Run");
+			System.out.println("\n------------------------------------------------------------------\n");
+			System.out.print("Select: ");
+			choice = s.nextInt();
 		
+			if(choice==1)
+			{
+				fight();
+			}
+			
+			else if (choice == 2)
+			{
+			System.out.println("You can't run!" + monster[monsterRandomizer] + " killed you");
+			}
+		}
 	}
 	
 	
 	
+	public void fight()
+	{
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("\t>Your HP: "+ playerHP);
+		System.out.println("\t>Monster HP: " + monsterHP);
+		System.out.println("Choose:");
+		System.out.println("\n1: Attack");
+		System.out.println("2: Run");
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.print("Select: ");
+		choice = s.nextInt();
+		
+		if(choice==1)
+		{
+			attack();
+		}
+		else if(choice==2)
+		{
+			System.out.println("You can't run!" + monster[monsterRandomizer] + " killed you");
+		}
+		else
+		{
+			fight();
+		}
+	}
+	
 	public void attack()
 	{
-		
-		int playerDamage = r.nextInt(50);
-		
-	
-		
+		int playerDamage = r.nextInt(25);
+		//int newMonsterHP; FOR TESTING PURPOSES
+		System.out.println("");
+		System.out.println(monsterCounter + " enemies left!");
 		System.out.println("You attacked the monster and gave " + playerDamage + " damage!");
-		int monsterHP = 50;
+		
 		monsterHP = monsterHP - playerDamage;
 		
 		System.out.println("Monster HP: " + monsterHP);
+		System.out.println("Please enter (1) to continue");
 		
 		if(monsterHP<1)
-		{ 
-			monsterKilled(); 
+		{
+			win(); 
 		} 
 		
 		else if(monsterHP>0)
@@ -119,57 +161,61 @@ public class Prototype
 			System.out.println("Player HP: " + playerHP);
 			
 			if(playerHP<1)
-			{
+			{ 
 				dead(); 
 			} 
 			
 			else if(playerHP>0)
 			{
-				attack();
+				fight();
 			}
 		}
 		
 		
 	}
 	
-	
-	public void monsterKilled()
-	{
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.println("You killed the monster!");
-		System.out.println("The monster dropped the key towards the dungeon!");
-		System.out.println("You obtained the key!\n\n");
-		System.out.println("1. Go to dungeon");
-		System.out.println("2. Exit");
-		subChoice = s.nextInt();
-		
-		key = 1;
-		
-		if (subChoice == 1 )
-		{
-			bossFight();
-		}
-		
-		else 
-		{
-			System.out.println("You left the game. You FAILED");
-		}
-	
-	}
-	
-	
 	public void dead()
 	{
-		System.out.println("You died game over.");
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You are dead!!!");
+		System.out.println("\n\nGAME OVER");
+		System.out.println("\n------------------------------------------------------------------\n");
+		
 	}
 	
-	public void bossFight()
+	public void win()
 	{
-		//System.out.println("Test");
-		s.nextLine();
+		if(monsterCounter == 0)
+		{
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("You killed all the monster!");
+		System.out.println("The monster dropped a key to the dungeon!");
+		System.out.println("You obtaind a key!\n\n");
+		System.out.println("1: Continue to the dungeon");
+		System.out.println("\n------------------------------------------------------------------\n");
+		}
+		key = 1;
+		
+		choice = s.nextInt();
+		if(choice==1)
+		{
+			//SOON 
+			//TO ENTER OR TO GO TO DUNGEON TO FIGHT THE BOSS
+		}
+		else
+		{
+			win();
+		}
+		
 	}
 	
 	public void ending()
 	{
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println("Work");
+		System.out.println("in");
+		System.out.println("Progress ");
+		System.out.println("\n------------------------------------------------------------------\n");
 	}
-}	
+	
+}
